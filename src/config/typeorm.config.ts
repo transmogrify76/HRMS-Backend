76131e4278d3a094603ea1c79ea4af0c3a9834c4
Chitradeep <transev76@gmail.com>
2024-04-12 10:00:40 +0530

    DB changes to Chit-Local

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -5,8 +5,8 @@ export const typeOrmConfig: TypeOrmModuleOptions = {
   host: '127.0.0.1',
   port: 3306,
   username: 'root',
-  password: 'admin',
-  database: 'hrms',
+  password: 'Chitradeep@12345',
+  database: 'hrms4',
   entities: [
     __dirname + '/../**/*.entity{.ts,.js}',
   ],
