cd52e1ff6aefc33faf7926184b3d2b48c1a59389
Chitradeep <transev76@gmail.com>
2024-04-18 17:25:25 +0530

    forlive

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -1,41 +1,29 @@
 import { TypeOrmModuleOptions } from "@nestjs/typeorm";
 
-
-
-
-//Production DB
-
-
-// export const typeOrmConfig: TypeOrmModuleOptions = {
-//   type: 'mysql',
-//   host: '154.41.233.52',
-//   port: 3306,
-//   username: 'u179156391_transhrms',
-//   password: 'Tgpl@2023',
-//   database: 'u179156391_hrms',
-//   entities: [ __dirname + '/../**/*.entity{.ts,.js}',],
-//   synchronize: false,
-//   logging: true
-// };
-
-
-
-
-
-
-//Local DB
-
-
 export const typeOrmConfig: TypeOrmModuleOptions = {
   type: 'mysql',
-  host: '127.0.0.1',
+  host: '154.41.233.52',
   port: 3306,
-  username: 'root',
-  password: 'admin',
-  database: 'hrms',
+  username: 'u179156391_transhrms',
+  password: 'Tgpl@2023',
+  database: 'u179156391_hrms',
   entities: [
     __dirname + '/../**/*.entity{.ts,.js}',
   ],
-  synchronize: false,
+  synchronize: true,
   logging: true
 };
+
+// export const typeOrmConfig: TypeOrmModuleOptions = {
+//   type: 'mysql',
+//   host: '127.0.0.1',
+//   port: 3306,
+//   username: 'root',
+//   password: 'Chitradeep@12345',
+//   database: 'hrms4',
+//   entities: [
+//     __dirname + '/../**/*.entity{.ts,.js}',
+//   ],
+//   synchronize: false,
+//   logging: true
+// };
