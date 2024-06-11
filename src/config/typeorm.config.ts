0910b326ed0650fcfc84a417661a07c5acaac003
ShubhamPrasad-TransEV <transmogrify11@outlook.com>
2024-06-11 09:43:35 +0530

    Production DB

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -6,32 +6,32 @@ import { Image } from "src/modules/upload-pic/image.entity";
  //Production DB
 
 
-// export const typeOrmConfig: TypeOrmModuleOptions = {
-//   type: 'mysql',
-//   host: '154.41.233.52',
-//   port: 3306,
-//   entities: [ __dirname + '/../**/*.entity{.ts,.js}',],
-//   username: 'u179156391_transhrms',
-//   password: 'Tgpl@2023',
-//   database: 'u179156391_hrms',
-//   synchronize: false,
-//   logging: true
-// };
+export const typeOrmConfig: TypeOrmModuleOptions = {
+  type: 'mysql',
+  host: '154.41.233.52',
+  port: 3306,
+  entities: [ __dirname + '/../**/*.entity{.ts,.js}',],
+  username: 'u179156391_transhrms',
+  password: 'Tgpl@2023',
+  database: 'u179156391_hrms',
+  synchronize: false,
+  logging: true
+};
 
 
 
 // Local DB
-export const typeOrmConfig: TypeOrmModuleOptions = {
-  type: 'mysql',
-  host: '127.0.0.1',
-  port: 3306,
-  username: 'root',
-  password: 'admin',
-  database: 'hrms',
-  entities: [
-    Image,
-    __dirname + '/../**/*.entity{.ts,.js}' ,
-  ],
-  synchronize: false,
-  logging: true
-};
+// export const typeOrmConfig: TypeOrmModuleOptions = {
+//   type: 'mysql',
+//   host: '127.0.0.1',
+//   port: 3306,
+//   username: 'root',
+//   password: 'admin',
+//   database: 'hrms',
+//   entities: [
+//     Image,
+//     __dirname + '/../**/*.entity{.ts,.js}' ,
+//   ],
+//   synchronize: false,
+//   logging: true
+// };
